aa1869e56dddf721bfe888405e3c33bab71c6112
suark <suark.nairb@gmail.com>
2023-11-30 00:45:24 -0700

    Calculations and tests all working. Reorganization and cleanup

---
+++ src/utils/calculate.test.ts
@@ -0,0 +1,41 @@
+import { reversePolishCalculate } from './calculate'
+
+const input1 = '10 3 +'
+const answer1 = 13
+const input2 = '10 3 2 + -'
+const answer2 = 5
+const input3 = '10 3 * 2 ^'
+const answer3 = 900
+const input4 = '3 2 /'
+const answer4 = 1.5
+const input5 = '29 7 6 * - 5 + 92 + 2 /'
+const meaningOfLife = 42
+const badInput = 'C + + is a cool language, but not a RPN equation'
+
+describe('reversePolishCalculate', () => {
+  it(`returns the correct result: ${answer1}, for input: ${input1}`, () => {
+    expect(reversePolishCalculate(input1)).toEqual(answer1)
+  })
+
+  it(`returns the correct result: ${answer2}, for input: ${input2}`, () => {
+    expect(reversePolishCalculate(input2)).toEqual(answer2)
+  })
+
+  it(`returns the correct result: ${answer3}, for input: ${input3}`, () => {
+    expect(reversePolishCalculate(input3)).toEqual(answer3)
+  })
+
+  it(`returns the correct result: ${answer4}, for input: ${input4}`, () => {
+    expect(reversePolishCalculate(input4)).toEqual(answer4)
+  })
+
+  it(`returns the correct result: ${meaningOfLife}, for input: ${input5}`, () => {
+    expect(reversePolishCalculate(input5)).toEqual(meaningOfLife)
+  })
+
+  it(`throw error: Error evaluating improper Reverse Polish Notation, for bad input: ${badInput}`, () => {
+    expect(() => {
+      reversePolishCalculate(badInput)
+    }).toThrow('Error evaluating improper Reverse Polish Notation')
+  })
+})
---
+++ src/utils/calculate.ts
@@ -0,0 +1,52 @@
+const possibleOperands = ['^', '*', '/', '+', '-']
+const operands = ['^', '*', '/', '+', '-'] as const
+type Operand = (typeof operands)[number]
+
+function isOperand(operandString: string): operandString is Operand {
+  return possibleOperands.includes(operandString)
+}
+
+function evaluate(left: string, right: string, operand: Operand) {
+  const leftAsNumber = parseFloat(left)
+  const rightAsNumber = parseFloat(right)
+  switch (operand) {
+    case '^':
+      return leftAsNumber ** rightAsNumber
+    case '*':
+      return leftAsNumber * rightAsNumber
+    case '/':
+      return leftAsNumber / rightAsNumber
+    case '+':
+      return leftAsNumber + rightAsNumber
+    case '-':
+      return leftAsNumber - rightAsNumber
+  }
+}
+
+export function reversePolishCalculate(equation: string): number | undefined {
+  try {
+    const equationArray = equation.split(' ')
+
+    if (equationArray.length === 1) {
+      return parseFloat(equation)
+    }
+
+    for (let i = 0; i < equationArray.length; i++) {
+      const operatorOrOperand = equationArray[i]
+      if (isOperand(operatorOrOperand)) {
+        const left = equationArray[i - 2]
+        const right = equationArray[i - 1]
+
+        if (left == null || right == null) {
+          throw new Error('Invalid Operators')
+        }
+
+        const result = evaluate(left, right, operatorOrOperand)
+        equationArray.splice(i - 2, 3, result.toString())
+        return reversePolishCalculate(equationArray.join(' '))
+      }
+    }
+  } catch (error) {
+    throw new Error('Error evaluating improper Reverse Polish Notation', { cause: error })
+  }
+}
